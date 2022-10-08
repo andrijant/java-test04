898a5c0640cd4c75013cbbb1fe96608f04283db7
andrijant <andrija_ds@yahoo.com>
2022-10-08 17:27:25 +0200

    arithmetic operators .sqrt .pow .abs

--- src/javaapplication4/Test4.java
+++ src/javaapplication4/Test4.java
@@ -6,14 +6,22 @@ public class Test4 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        // Function F(x, y) = x * y  /  1 - x  + 3
-        
-        System.out.print("Enter 'x' and 'y' values: ");
+                
+        System.out.print("Enter number: ");
         double x = s.nextDouble();
-        double y = s.nextDouble();
-        double f = x * y / (1 - x) + 3;
         
-        System.out.println("F(x, y) = " + f);
+        double f1 = Math.sqrt(x);  // square root         
+        System.out.println("Square root of number " + x + " is: " + f1);
+       
+        double f2 = Math.pow(x, 3.0);  // power of number
+        System.out.println("Number " + x + " to the power of 3 is: " + f2);
+              
+        double f3 = Math.abs(x);
+        System.out.println("Absolute value of number " + x + " is: " + f3);
+        
+        double f4 = Math.PI * x;
+        System.out.println("PI value of number " + x + " is: " + f4);
+        
     }
     
 }
